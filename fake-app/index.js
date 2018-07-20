b22d2b56b1a29a8dc01f8c8424d40d28fed1a2ee
Goll Araidah <jaraidah@souq.com>
2018-07-20 23:56:57 +0300

    Awwal commit

---
+++ fake-app/index.js
@@ -0,0 +1,9 @@
+var fetch = require("node-fetch");
+
+fetch(`https://jsonplaceholder.typicode.com/posts/1`)
+  .then(response => response.json())
+  .then(json => console.log(json))
+  
+
+console.log('asdas');
+
